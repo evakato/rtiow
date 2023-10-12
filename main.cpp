c9ec48f85f03df0b6870725580b793a317e18985
Eva Kato <evakato14@gmail.com>
2023-10-12 11:23:09 +0200

    simple red sphere

--- main.cpp
+++ main.cpp
@@ -3,7 +3,19 @@
 #include "ray.h"
 #include "color.h"
 
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = r.origin() - center;
+    auto a = dot(r.direction(), r.direction());
+    auto b = 2.0 * dot(oc, r.direction());
+    auto c = dot(oc, oc) - radius*radius;
+    auto discriminant = b*b - 4*a*c;
+    return (discriminant >= 0);
+}
+
 color ray_color(const ray& r) {
+    if (hit_sphere(point3(0,0,-1), 0.5, r))
+        return color(1, 0, 0);
+
     // scale the ray direction to unit length -1.0 < y < 1.0
     vec3 unit_direction = unit_vector(r.direction());
     // lerp white and blue

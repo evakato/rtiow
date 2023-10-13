70f2b28ac3949b6d88540c3c4849e56626708450
Eva Kato <evakato14@gmail.com>
2023-10-13 15:32:33 +0200

    antialiasing

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@
 #include "sphere.h"
 
 int main() {
-
     // World
     hittable_list world;
     world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
@@ -13,6 +12,6 @@ int main() {
     camera cam;
     cam.aspect_ratio = 16.0 / 9.0;
     cam.image_width = 400;
+    cam.samples_per_pixel = 100;
     cam.render(world);
-
 }
